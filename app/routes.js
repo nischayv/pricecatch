1b070ad2873580033c673c0e518337caddac79bf
Nischay Venkatram <nischay.v@gmail.com>
2017-04-18 21:03:39 -0700

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//Routes go here
+
+module.exports = router;
+
+
+// Example endpoint
+
+// router.get('/', function(req, res) {
+//   res.sendFile('index.html');
+// });
