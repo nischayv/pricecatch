44db2a0326130f64db5fde2b82ba5f24c0485dac
Nischay Venkatram <nischay.v@gmail.com>
2017-04-18 21:26:25 -0700

    Add Amazon price scrapper

---
+++ scrapper.js
@@ -0,0 +1,16 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+function getPrice(url, cb) {
+
+  request(url, (err, res, html) => {
+    if (err) {
+      return cb(err, null);
+    }
+    const $ = cheerio.load(html);
+    const price = $('#priceblock_ourprice_row').find('#priceblock_ourprice') text();
+    return cb(null, price);
+  });
+}
+
+module.exports.getPrice = getPrice;
