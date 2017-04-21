132086ae8aefad0d59d6e92439f85c1d8c54e762
Nischay Venkatram <nischay.v@gmail.com>
2017-04-21 00:31:52 -0700

    Fix broken material ui

--- app/app.js
+++ app/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppContainer from './Container'
+
+injectTapEventPlugin();
 
 class App extends React.Component {
   render() {
     return (
-      <AppContainer />
+      <MuiThemeProvider>
+        <AppContainer />
+      </MuiThemeProvider>
     );
   }
 }
